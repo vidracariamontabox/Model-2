5dbc04c49f5c0eefdf98d4f4dc16c89ae10f2170
betioli <williambetioli@hotmail.com>
2026-07-01 12:45:35 -0300

    Nova seção

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,8 @@
 import Navbar from '@/components/Navbar';
 import Hero from '@/components/Hero';
 import About from '@/components/About';
-import TestimonialsCarousel from '@/components/TestimonialsCarousel';
-import { testimonials } from '@/data/testimonials';
+import Testimonials from '@/components/Testimonials/Testimonials';  // Nova seção
 import FAQ from '@/components/FAQ';
-import { faqs } from '@/data/faq';
 
 export default function Page() {
   return (
@@ -12,8 +10,8 @@ export default function Page() {
       <Navbar />
       <Hero />
       <About />
-      <TestimonialsCarousel testimonials={testimonials} />
-      <FAQ faqs={faqs} />
+      <Testimonials />           {/* Aqui está a nova seção */}
+      <FAQ />
     </main>
   );
 }
