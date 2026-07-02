c4939de5c891018f7392e763c59a138809d2bd13
betioli <williambetioli@hotmail.com>
2026-07-02 16:03:01 -0300

    Ignorar pasta .next e limpar cache

--- .next/dev/static/chunks/app_page_jsx_0wu2ehs._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_motion-dom_dist_es_1kh_q9w._.js","static/chunks/node_modules_framer-motion_dist_es_0bp0ex4._.js","static/chunks/node_modules_swiper_03x7mxd._.js","static/chunks/node_modules_gsap_04cg7u0._.js","static/chunks/node_modules_1p7aybo._.js","static/chunks/_0uc7456._.js"],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/app_page_jsx_1owx-gc._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_0hyp4_j._.js","static/chunks/node_modules_motion-dom_dist_es_1kh_q9w._.js","static/chunks/node_modules_framer-motion_dist_es_0bp0ex4._.js","static/chunks/node_modules_swiper_03x7mxd._.js","static/chunks/node_modules_gsap_04cg7u0._.js","static/chunks/node_modules_1p7aybo._.js"],
-    source: "dynamic"
-});
