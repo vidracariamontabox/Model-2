cef5949ddbc26fd8e5cc30b58f563fc1785c0348
Betioli <1.3007332e+08+Betioli@users.noreply.github.com>
2026-08-29 17:53:41 +0000

    perf: remove legacy browser polyfill module

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,19 @@
+import path from "node:path";
+import webpack from "next/dist/compiled/webpack/webpack-lib.js";
+
 const nextConfig = {
   productionBrowserSourceMaps: true,
+  webpack: (config, {isServer}) => {
+    if (!isServer) {
+      config.plugins.push(
+        new webpack.NormalModuleReplacementPlugin(
+          /next[\\/]dist[\\/]build[\\/]polyfills[\\/]polyfill-module\.js$/,
+          path.resolve("empty-polyfill.js"),
+        ),
+      );
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
